63eb0aaf5f5bb0d70b308cdaab48308dacbe2a08
Nithish Kumar <nithishindians@gmail.com>
2023-03-04 22:57:51 +0530

    just commiting with link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>My home page</div>
+    <div>My home page
+      <p>
+        Go to <Link to="/products" >the list of products</Link>
+      </p>
+    </div>
   )
 }
 
